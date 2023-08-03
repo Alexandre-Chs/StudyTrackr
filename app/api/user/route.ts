f8fb142c6cb2c7ff42ce22ef84ed10c70dce0383
Alexandre-Chs <oliuuum@gmail.com>
2023-08-03 16:40:24 +0200

    modify route.ts cauz vercel is not happy

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,1 +1,8 @@
-// EXAMPLE ROUTE
+import { NextResponse } from "next/server";
+// EXAMPLE CODE
+export async function GET() {
+  const res = await fetch("https://data.mongodb-api.com/...");
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
